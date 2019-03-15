76380e1bd9f3195ecb90de5cbdbb56cda56e4cfb
Sergei Orlov <priestine1.dev@gmail.com>
2019-03-15 19:42:03 +1000

    feat(check accept header): pass if */* is acceptable

--- src/middleware/request/validation/check-accept-header.ts
+++ src/middleware/request/validation/check-accept-header.ts
@@ -1,13 +1,21 @@
-import { HttpContextInterface } from "@priestine/routing";
-import { NotAcceptableError } from "../../../errors";
+import { HttpContextInterface } from '@priestine/routing';
+import { NotAcceptableError } from '../../../errors';
 
 /**
  * Check if accept header is defined and supported.
  *
  * @throws NotAcceptableError
  */
-export const CheckAcceptHeader = (accepted: string[] = ['*/*'], message?: string) => ({ request }: HttpContextInterface): void => {
-  if (!request.headers.accept || !accepted.includes(request.headers.accept)) {
+export const CheckAcceptHeader = (acceptable: string[] = ['*/*'], message?: string) => ({
+  request,
+}: HttpContextInterface): void => {
+  if (!request.headers.accept) {
     throw NotAcceptableError.withMessage(message || 'not acceptable');
   }
+
+  if (acceptable.includes('*/*') || acceptable.includes(request.headers.accept)) {
+    return;
+  }
+
+  throw NotAcceptableError.withMessage(message || 'not acceptable');
 };
